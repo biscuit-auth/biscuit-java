5da18216b12f24a0259c6e4cae5ae873cf30d6c3
Geoffroy Couprie <geo.couprie@gmail.com>
2019-09-11 17:02:56 +0200

    test for an expired token

--- src/test/java/com/clevercloud/biscuit/token/SamplesTest.java
+++ src/test/java/com/clevercloud/biscuit/token/SamplesTest.java
@@ -4,6 +4,7 @@ import cafe.cryptography.curve25519.CompressedRistretto;
 import cafe.cryptography.curve25519.InvalidEncodingException;
 import cafe.cryptography.curve25519.RistrettoElement;
 import com.clevercloud.biscuit.error.Error;
+import com.clevercloud.biscuit.error.FailedCaveat;
 import com.clevercloud.biscuit.error.LogicError;
 import io.vavr.control.Either;
 import junit.framework.Assert;
@@ -14,6 +15,7 @@ import sun.misc.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import static com.clevercloud.biscuit.crypto.TokenSignature.fromHex;
 import static com.clevercloud.biscuit.crypto.TokenSignature.hex;
@@ -68,7 +70,6 @@ public class SamplesTest extends TestCase {
         InputStream inputStream =
                 Thread.currentThread().getContextClassLoader().getResourceAsStream("test2_different_root_key.bc");
 
-        System.out.println("a");
         byte[] data = new byte[inputStream.available()];
         inputStream.read(data);
 
@@ -84,12 +85,36 @@ public class SamplesTest extends TestCase {
         InputStream inputStream =
                 Thread.currentThread().getContextClassLoader().getResourceAsStream("test3_invalid_signature.bc");
 
-        System.out.println("a");
         byte[] data = new byte[inputStream.available()];
         inputStream.read(data);
 
         Error e = Biscuit.from_bytes(data, root).getLeft();
         System.out.println("got error: "+ e);
         Assert.assertEquals(new Error().new FormatError().new Signature().new InvalidSignature(), e);
+    }
+
+    public void test4_ExpiredToken() throws IOException, InvalidEncodingException {
+        byte[] rootData = fromHex("da905388864659eb785877a319fbc42c48e2f8a40af0c5baea0ef8ff7c795253");
+        RistrettoElement root = (new CompressedRistretto(rootData)).decompress();
+
+        InputStream inputStream =
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("test4_expired_token.bc");
+
+        byte[] data = new byte[inputStream.available()];
+        inputStream.read(data);
+
+        Biscuit token = Biscuit.from_bytes(data, root).get();
+        System.out.println(token.print());
+
+        Verifier v1 = new Verifier();
+        v1.resource("file1");
+        v1.operation("read");
+        v1.time();
+        LogicError e = v1.verify(token).getLeft();
+        Assert.assertEquals(new Error().new FailedLogic(
+                new LogicError().new FailedCaveats(Arrays.asList(
+                        new FailedCaveat().new FailedBlock(0, 1, "expiration(0?) <- time(#ambient, 0?) | 0? <= 1545264000")
+                ))
+        ), e);
     }
 }
