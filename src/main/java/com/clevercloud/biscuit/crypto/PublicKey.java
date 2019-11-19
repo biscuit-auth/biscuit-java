a7b44464749d69a594ea3caffdc161f9144dee55
Geoffroy Couprie <geo.couprie@gmail.com>
2019-11-19 14:44:50 +0100

    public key constructor from byte array

--- src/main/java/com/clevercloud/biscuit/crypto/PublicKey.java
+++ src/main/java/com/clevercloud/biscuit/crypto/PublicKey.java
@@ -1,5 +1,7 @@
 package com.clevercloud.biscuit.crypto;
 
+import cafe.cryptography.curve25519.CompressedRistretto;
+import cafe.cryptography.curve25519.InvalidEncodingException;
 import cafe.cryptography.curve25519.RistrettoElement;
 
 public class PublicKey {
@@ -8,5 +10,10 @@ public class PublicKey {
 
     public PublicKey(RistrettoElement public_key) {
         this.key = public_key;
+    }
+
+    public PublicKey(byte[] data) throws InvalidEncodingException {
+        CompressedRistretto c = new CompressedRistretto(data);
+        this.key = c.decompress();
     }
 }
