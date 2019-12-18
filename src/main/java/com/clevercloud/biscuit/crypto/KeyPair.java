d071cafe2f6db0922b291f71df063b49bd34265f
Geoffroy Couprie <geo.couprie@gmail.com>
2019-12-18 12:06:38 +0100

    hex conversion functions

--- src/main/java/com/clevercloud/biscuit/crypto/KeyPair.java
+++ src/main/java/com/clevercloud/biscuit/crypto/KeyPair.java
@@ -30,7 +30,38 @@ public final class KeyPair {
         this.public_key = Constants.RISTRETTO_GENERATOR.multiply(this.private_key);
     }
 
+    public String toHex() {
+        return byteArrayToHexString(this.toBytes());
+    }
+
+    public KeyPair(String hex) {
+        byte[] b = hexStringToByteArray(hex);
+        this.private_key = Scalar.fromBytesModOrderWide(b);
+        this.public_key = Constants.RISTRETTO_GENERATOR.multiply(this.private_key);
+    }
+
     public PublicKey public_key() {
         return new PublicKey(this.public_key);
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    static String byteArrayToHexString(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    static byte[] hexStringToByteArray(String hex) {
+        int l = hex.length();
+        byte[] data = new byte[l/2];
+        for (int i = 0; i < l; i += 2) {
+            data[i/2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                    + Character.digit(hex.charAt(i+1), 16));
+        }
+        return data;
     }
 }
--- src/main/java/com/clevercloud/biscuit/crypto/PublicKey.java
+++ src/main/java/com/clevercloud/biscuit/crypto/PublicKey.java
@@ -20,4 +20,35 @@ public class PublicKey {
     public byte[] toBytes() {
         return this.key.compress().toByteArray();
     }
+
+    public String toHex() {
+        return byteArrayToHexString(this.toBytes());
+    }
+
+    public PublicKey(String hex) throws InvalidEncodingException {
+        byte[] data = hexStringToByteArray(hex);
+        CompressedRistretto c = new CompressedRistretto(data);
+        this.key = c.decompress();
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    static String byteArrayToHexString(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    static byte[] hexStringToByteArray(String hex) {
+        int l = hex.length();
+        byte[] data = new byte[l/2];
+        for (int i = 0; i < l; i += 2) {
+            data[i/2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                    + Character.digit(hex.charAt(i+1), 16));
+        }
+        return data;
+    }
 }
