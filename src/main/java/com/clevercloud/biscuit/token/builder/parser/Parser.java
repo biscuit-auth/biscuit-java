1fbb7c85c2bcdd99c9065266332eea972d94d675
Geoffroy Couprie <contact@geoffroycouprie.com>
2020-12-18 15:49:47 +0100

    missing case in the date parser

--- src/main/java/com/clevercloud/biscuit/token/builder/parser/Parser.java
+++ src/main/java/com/clevercloud/biscuit/token/builder/parser/Parser.java
@@ -323,7 +323,7 @@ public class Parser {
         int index = s.length();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == ' ' || c == ',') {
+            if (c == ' ' || c == ',' || c == ')') {
                 index = i;
                 break;
             }
