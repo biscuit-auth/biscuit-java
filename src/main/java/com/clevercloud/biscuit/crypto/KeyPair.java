d2c6960c38df095e702b37e2c99d82e240413f59
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-03-23 10:51:15 +0100

    use the hexadecimal string conversions from Utils

--- src/main/java/com/clevercloud/biscuit/crypto/KeyPair.java
+++ src/main/java/com/clevercloud/biscuit/crypto/KeyPair.java
@@ -3,6 +3,7 @@ package com.clevercloud.biscuit.crypto;
 import cafe.cryptography.curve25519.Constants;
 import cafe.cryptography.curve25519.RistrettoElement;
 import cafe.cryptography.curve25519.Scalar;
+import com.clevercloud.biscuit.token.builder.Utils;
 
 import java.security.SecureRandom;
 
@@ -31,37 +32,16 @@ public final class KeyPair {
     }
 
     public String toHex() {
-        return byteArrayToHexString(this.toBytes());
+        return Utils.byteArrayToHexString(this.toBytes());
     }
 
     public KeyPair(String hex) {
-        byte[] b = hexStringToByteArray(hex);
+        byte[] b = Utils.hexStringToByteArray(hex);
         this.private_key = Scalar.fromBytesModOrder(b);
         this.public_key = Constants.RISTRETTO_GENERATOR.multiply(this.private_key);
     }
 
     public PublicKey public_key() {
         return new PublicKey(this.public_key);
-    }
-
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-    static String byteArrayToHexString(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-    static byte[] hexStringToByteArray(String hex) {
-        int l = hex.length();
-        byte[] data = new byte[l/2];
-        for (int i = 0; i < l; i += 2) {
-            data[i/2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
-                    + Character.digit(hex.charAt(i+1), 16));
-        }
-        return data;
     }
 }
--- src/main/java/com/clevercloud/biscuit/crypto/PublicKey.java
+++ src/main/java/com/clevercloud/biscuit/crypto/PublicKey.java
@@ -3,6 +3,7 @@ package com.clevercloud.biscuit.crypto;
 import cafe.cryptography.curve25519.CompressedRistretto;
 import cafe.cryptography.curve25519.InvalidEncodingException;
 import cafe.cryptography.curve25519.RistrettoElement;
+import com.clevercloud.biscuit.token.builder.Utils;
 
 public class PublicKey {
     public final RistrettoElement key;
@@ -22,33 +23,12 @@ public class PublicKey {
     }
 
     public String toHex() {
-        return byteArrayToHexString(this.toBytes());
+        return Utils.byteArrayToHexString(this.toBytes());
     }
 
     public PublicKey(String hex) throws InvalidEncodingException {
-        byte[] data = hexStringToByteArray(hex);
+        byte[] data = Utils.hexStringToByteArray(hex);
         CompressedRistretto c = new CompressedRistretto(data);
         this.key = c.decompress();
-    }
-
-    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-    static String byteArrayToHexString(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-    static byte[] hexStringToByteArray(String hex) {
-        int l = hex.length();
-        byte[] data = new byte[l/2];
-        for (int i = 0; i < l; i += 2) {
-            data[i/2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
-                    + Character.digit(hex.charAt(i+1), 16));
-        }
-        return data;
     }
 }
