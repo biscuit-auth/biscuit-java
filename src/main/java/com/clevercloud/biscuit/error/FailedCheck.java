381069819ffa3a741b82cf411ac0ce8f1fd9794c
Cedric Corbiere <corbic@gmail.com>
2022-02-10 11:19:03 +0100

    Change caveat_id to check_id

--- src/main/java/com/clevercloud/biscuit/error/FailedCheck.java
+++ src/main/java/com/clevercloud/biscuit/error/FailedCheck.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class FailedCheck {
     public static class FailedBlock extends FailedCheck {
         final public long block_id;
-        final public long caveat_id;
+        final public long check_id;
         final public String rule;
 
-        public FailedBlock(long block_id, long caveat_id, String rule) {
+        public FailedBlock(long block_id, long check_id, String rule) {
             this.block_id = block_id;
-            this.caveat_id = caveat_id;
+            this.check_id = check_id;
             this.rule = rule;
         }
 
@@ -19,27 +19,27 @@ public class FailedCheck {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             FailedBlock b = (FailedBlock) o;
-            return block_id == b.block_id && caveat_id == b.caveat_id && rule.equals(b.rule);
+            return block_id == b.block_id && check_id == b.check_id && rule.equals(b.rule);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(block_id, caveat_id, rule);
+            return Objects.hash(block_id, check_id, rule);
         }
 
         @Override
         public String toString() {
-            return "FailedCheck.FailedBlock { block_id: " + block_id + ", caveat_id: "+caveat_id+
+            return "FailedCheck.FailedBlock { block_id: " + block_id + ", check_id: "+check_id+
                     ", rule: "+rule+" }";
         }
     }
 
     public static class FailedAuthorizer extends FailedCheck {
-        final public long caveat_id;
+        final public long check_id;
         final public String rule;
 
-        public FailedAuthorizer(long caveat_id, String rule) {
-            this.caveat_id = caveat_id;
+        public FailedAuthorizer(long check_id, String rule) {
+            this.check_id = check_id;
             this.rule = rule;
         }
 
@@ -48,17 +48,17 @@ public class FailedCheck {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             FailedAuthorizer b = (FailedAuthorizer) o;
-            return caveat_id == b.caveat_id && rule.equals(b.rule);
+            return check_id == b.check_id && rule.equals(b.rule);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(caveat_id, rule);
+            return Objects.hash(check_id, rule);
         }
 
         @Override
         public String toString() {
-            return "FailedCaveat.FailedAuthorizer { caveat_id: "+caveat_id+
+            return "FailedCaveat.FailedAuthorizer { check_id: "+check_id+
                     ", rule: "+rule+" }";
         }
     }
