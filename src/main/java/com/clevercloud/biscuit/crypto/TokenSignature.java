e22b7df85cfc2c8036a5f4d026ff19dcf0d6b17b
Scott Robinson <ssr@squareup.com>
2023-09-02 22:22:08 +1000

    cleanup: reuse hexStringToByteArray in TokenSignature

--- src/main/java/com/clevercloud/biscuit/crypto/TokenSignature.java
+++ src/main/java/com/clevercloud/biscuit/crypto/TokenSignature.java
@@ -191,12 +191,6 @@ public class TokenSignature {
     }
 
     public static byte[] fromHex(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
-        }
-        return data;
+        return com.clevercloud.biscuit.token.builder.Utils.hexStringToByteArray(hex);
     }
 }
