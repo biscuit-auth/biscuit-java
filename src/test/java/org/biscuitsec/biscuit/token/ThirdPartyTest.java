cabfeefc5bf40d51470c7a78d506097c6f03473c
ghubstan <36207203+ghubstan@users.noreply.github.com>
2024-09-12 16:29:38 -0300

    Use static import

--- src/test/java/org/biscuitsec/biscuit/token/ThirdPartyTest.java
+++ src/test/java/org/biscuitsec/biscuit/token/ThirdPartyTest.java
@@ -4,6 +4,7 @@ import org.biscuitsec.biscuit.crypto.KeyPair;
 import org.biscuitsec.biscuit.datalog.RunLimits;
 import org.biscuitsec.biscuit.error.Error;
 import org.biscuitsec.biscuit.error.FailedCheck;
+import org.biscuitsec.biscuit.error.FailedCheck.FailedBlock;
 import org.biscuitsec.biscuit.token.builder.Block;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,7 @@ import java.util.List;
 
 import static java.lang.System.out;
 import static org.biscuitsec.biscuit.error.LogicError.MatchedPolicy;
+import static org.biscuitsec.biscuit.error.LogicError.MatchedPolicy.*;
 import static org.biscuitsec.biscuit.error.LogicError.Unauthorized;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -73,8 +75,8 @@ public class ThirdPartyTest {
         } catch (Error e) {
             out.println(e);
             assertEquals(
-                    new Error.FailedLogic(new Unauthorized(new MatchedPolicy.Allow(0), List.of(
-                            new FailedCheck.FailedBlock(1, 0, "check if resource(\"file1\")")
+                    new Error.FailedLogic(new Unauthorized(new Allow(0), List.of(
+                            new FailedBlock(1, 0, "check if resource(\"file1\")")
                     ))),
                     e);
         }
@@ -157,8 +159,8 @@ public class ThirdPartyTest {
         } catch (Error e) {
             out.println(e);
             assertEquals(
-                    new Error.FailedLogic(new Unauthorized(new MatchedPolicy.Allow(0), List.of(
-                            new FailedCheck.FailedBlock(3, 0, "check if resource(\"file1\")")
+                    new Error.FailedLogic(new Unauthorized(new Allow(0), List.of(
+                            new FailedBlock(3, 0, "check if resource(\"file1\")")
                     ))),
                     e);
         }
@@ -212,8 +214,8 @@ public class ThirdPartyTest {
         } catch (Error e) {
             out.println(e);
             assertEquals(
-                    new Error.FailedLogic(new Unauthorized(new MatchedPolicy.Allow(0), List.of(
-                            new FailedCheck.FailedBlock(1, 0, "check if resource(\"file1\")")
+                    new Error.FailedLogic(new Unauthorized(new Allow(0), List.of(
+                            new FailedBlock(1, 0, "check if resource(\"file1\")")
                     ))),
                     e);
         }
