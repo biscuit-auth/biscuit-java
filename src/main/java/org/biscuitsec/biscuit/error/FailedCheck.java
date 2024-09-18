b329c8588cdc190152ef2d9c7bdf5d529c02338e
ghubstan <36207203+ghubstan@users.noreply.github.com>
2024-09-18 13:28:47 -0300

    Use camelCase field and param names in FailedCheck.java

--- src/main/java/org/biscuitsec/biscuit/error/FailedCheck.java
+++ src/main/java/org/biscuitsec/biscuit/error/FailedCheck.java
@@ -10,13 +10,13 @@ public class FailedCheck {
     public JsonElement toJson(){ return new JsonObject();}
 
     public static class FailedBlock extends FailedCheck {
-        final public long block_id;
-        final public long check_id;
+        final public long blockId;
+        final public long checkId;
         final public String rule;
 
-        public FailedBlock(long block_id, long check_id, String rule) {
-            this.block_id = block_id;
-            this.check_id = check_id;
+        public FailedBlock(long blockId, long checkId, String rule) {
+            this.blockId = blockId;
+            this.checkId = checkId;
             this.rule = rule;
         }
 
@@ -25,12 +25,12 @@ public class FailedCheck {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             FailedBlock b = (FailedBlock) o;
-            return block_id == b.block_id && check_id == b.check_id && rule.equals(b.rule);
+            return blockId == b.blockId && checkId == b.checkId && rule.equals(b.rule);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(block_id, check_id, rule);
+            return Objects.hash(blockId, checkId, rule);
         }
 
         @Override
@@ -41,8 +41,8 @@ public class FailedCheck {
         @Override
         public JsonElement toJson() {
             JsonObject jo = new JsonObject();
-            jo.addProperty("block_id", block_id);
-            jo.addProperty("check_id", check_id);
+            jo.addProperty("block_id", blockId);
+            jo.addProperty("check_id", checkId);
             jo.addProperty("rule", rule);
             JsonObject block = new JsonObject();
             block.add("Block", jo);
@@ -51,11 +51,11 @@ public class FailedCheck {
     }
 
     public static class FailedAuthorizer extends FailedCheck {
-        final public long check_id;
+        final public long checkId;
         final public String rule;
 
-        public FailedAuthorizer(long check_id, String rule) {
-            this.check_id = check_id;
+        public FailedAuthorizer(long checkId, String rule) {
+            this.checkId = checkId;
             this.rule = rule;
         }
 
@@ -64,24 +64,24 @@ public class FailedCheck {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             FailedAuthorizer b = (FailedAuthorizer) o;
-            return check_id == b.check_id && rule.equals(b.rule);
+            return checkId == b.checkId && rule.equals(b.rule);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(check_id, rule);
+            return Objects.hash(checkId, rule);
         }
 
         @Override
         public String toString() {
-            return "FailedCaveat.FailedAuthorizer { check_id: "+check_id+
+            return "FailedCaveat.FailedAuthorizer { check_id: "+ checkId +
                     ", rule: "+rule+" }";
         }
 
         @Override
         public JsonElement toJson() {
             JsonObject jo = new JsonObject();
-            jo.addProperty("check_id", check_id);
+            jo.addProperty("check_id", checkId);
             jo.addProperty("rule", rule);
             JsonObject authorizer = new JsonObject();
             authorizer.add("Authorizer", jo);
