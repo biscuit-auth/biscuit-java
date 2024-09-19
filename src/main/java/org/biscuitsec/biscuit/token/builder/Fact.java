ab4ac89f8da6dbf49ccbaedcea85ea817d67ae02
ghubstan <36207203+ghubstan@users.noreply.github.com>
2024-09-19 13:27:18 -0300

    Delete unused import

--- src/main/java/org/biscuitsec/biscuit/token/builder/Fact.java
+++ src/main/java/org/biscuitsec/biscuit/token/builder/Fact.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
